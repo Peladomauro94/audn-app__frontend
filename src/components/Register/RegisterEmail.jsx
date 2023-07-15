c64d3e37410bfd5fe1c15f1c3b38c54d516050f1
Peladomauro94 <121203878+Peladomauro94@users.noreply.github.com>
2023-07-14 22:33:23 -0300

    Register pages css ready

--- src/components/Register/RegisterEmail.jsx
+++ src/components/Register/RegisterEmail.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
 
-export const RegisterEmail = () => {
+import React, { useState } from 'react'
+
+export const RegisterEmail = ({ handleEmailValue, email, errorMsg }) => {
+
+  
+
+
+ 
+
   return (
-    <div className='register-email'>
-        <span className='register-email__question'>¿Cuál es tu correo electrónico?</span>
-        <form>
-            <label className='register-email__input-label' htmlFor="register-email__input">Correo electrónico:</label>
-            <input className='register-email__input' type="text" placeholder='example@audn.com'/>
-            <span></span>
+    <div className='register-content'>
+        <span className='register__question'>¿Cuál es tu correo electrónico?</span>
+        <form className='register__form'>
+            <label className='register__input-label' htmlFor="register-email__input">Correo electrónico:</label>
+            <input className='register__input' value={email} onChange={handleEmailValue} type="text" placeholder='example@audn.com'/>
+            <span className='register__input-span'>{errorMsg}</span>
         </form>
     </div>
   )
--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,24 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import { RegisterEmail } from './registerEmail'
 import { RegisterName } from './RegisterName'
-
+import './index.css'
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
     const [onView, setOnView] = useState (true);
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [nameError, setNameError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
 
-    useEffect(() =>{
-    
-    }, [onView])
+    const registerButtonHanndle = (e) =>{
+      e.preventDefault()
+      if (!email){
+        setEmailError('Please enter an email adress')
+      } else if ((/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i).test(email)){
+        setEmailError('')
+        setOnView(false)
+      } else {
+        setEmailError('Please enter a valid email adress')
+      }
+    }
+
+    const handleEmailValue = (e) =>{
+      const emailValue = e.target.value
+      setEmail(emailValue)
+      if (emailValue === ""){
+        setEmailError('')
+      } 
+    }
+
+    const handleNameValue = (e) =>{
+      const nameValue = e.target.value
+      setName(nameValue) 
+    }
+  
+    const handlePasswordValue = (e) =>{
+      const passwordValue = e.target.value
+      setPassword(passwordValue) 
+    }
+
 
   return (
     <div className='register__main-div'>
-        <div className='register__top'>
-            <img className='register__arrow-back' src="/arrow-left.svg" alt="" />
+
+        <div className='register__container'>
+          <div className='register__top'>
+            <Link to='/'><img className='register__arrow-back' src="/arrow-left.svg" alt="" /></Link>
             <span className='register__title'>Crear Cuenta</span>
-        </div>
-        { onView ? <RegisterEmail /> : <RegisterName />  }
-        
-        <button></button>
+          </div>
+            { onView ? <RegisterEmail value={email} handleEmailValue={handleEmailValue} errorMsg={emailError} /> : <RegisterName nameValue={name} passwordValue={password} handleNameValue={handleNameValue} PasswordValue={handlePasswordValue} nameError={nameError} passwordError={passwordError}/>  }
+          </div>
+
+        <button className='button-type1' onClick={registerButtonHanndle}>Continuar</button>
     </div>
   )
 }
