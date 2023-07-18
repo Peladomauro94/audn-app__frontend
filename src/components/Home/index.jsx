bb7b0733f392da26a4b3d577ad70be49e1120d28
EmiMinetti <eminetti99@gmail.com>
2023-07-17 22:29:24 -0300

    Avances home(usuarios)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,12 +5,24 @@ export const Home = () => {
   return (
     <div className='contHome'>
         <div className='titulosHome'>
-            <p>Música ya</p>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <span>Música ya</span>
+            <div className='iconosTit'>
+            <img src="/history.svg" alt="" />
+            <img src="/bell.svg" alt="" />
+            </div>
         </div>
-        <div>Cupido Musica</div>
-        <div>Música Contextual</div>
+        <div className='contBoxes'>
+            <div className='cupidoMusical'>
+                <img className='fotoBox' src="/cupido.svg" alt="" />
+                <p>Cupido Musical</p>
+                <p>Tus artistas favoritos nunca van a dejarte con el corazon roto.</p>            
+            </div>
+            <div className='cupidoMusical'>
+                <img className='fotoBox' src="/contextual.svg" alt="" />
+                <p>Música Contextual</p>
+                <p>Creamos la playlist perfecta para cualquier situación.</p>            
+            </div>
+        </div>
         <div>Barra</div>
     </div>
   )
