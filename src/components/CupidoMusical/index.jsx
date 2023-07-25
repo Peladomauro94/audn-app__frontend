bf97a0caad59628f2e6ebdab7198bbbffb71a8d8
EmiMinetti <eminetti99@gmail.com>
2023-07-25 18:52:10 -0300

    Avances

--- src/components/CupidoMusical/index.jsx
+++ src/components/CupidoMusical/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './index.css'
+import { Link } from 'react-router-dom'
 
 export const Cupido = () => {
   return (
     <div className='contCupido'>
         <div className='contTitle'>
-          <img className='flechaTitle' src="/vector.svg" alt="" />
+          <Link to='/home' className='flechaTitle'><img src="/vector.svg" alt="" /></Link>
           <p className='textCupido'>Cupido Musical</p>
+        </div>
+        <div className='contSongs'>
+          <img className='imgCupido' src="/neck-deep-image.png" alt="" />
+          <div>
+            <div><img src="/like-green.svg" alt="" /></div>
+            <div><img src="/cancel.svg" alt="" /></div>
+          </div>
+          <p>Neck Deep</p>
+        </div>
+        <div>
+          <button></button>
         </div>
     </div>
   )
