14c9a80f41f4e947dd006829fc05298309ec2b51
EmiMinetti <eminetti99@gmail.com>
2023-07-31 21:44:35 -0300

    busqueda reciente

--- src/components/Home/Sections/Searcher.jsx
+++ src/components/Home/Sections/Searcher.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from '../Card'
+import { useAuth } from '../../../contexts/authContext';
 
 export const Searcher = ({ searcher, topSongs }) => {
-  return (
-  <div className={`contSearcher${searcher} `}>
-  <div className='mainSearcher animation__insideRight'>
+
+    const [searchState, setSearchState] = useState('');
+    const [result, setResult] = useState([]);
+    const [onView, setOnView] = useState('top');
+
+    const {user} = useAuth();
+
+    const updateResult = (e) => {
+        e.preventDefault()
+        const searchQuery = encodeURIComponent(searchState)
+        fetch(`http://localhost:3000/songs/search?query=${searchQuery}`, {
+            headers:{'auth-token':user},
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+    }
+
+    const handleValue = (e) => {
+       const value = e.target.value
+       setSearchState(value)
+    }
+
+    const inputClickTop = () =>{
+        setOnView('search')
+    }
+
+    const inputClickSearch = () =>{
+        setOnView('top')
+    }
+
+
+  return (<div className={`contSearcher${searcher}`}>
+    {onView === 'top' && 
+    <>
+  <div className='mainSearcher animation__insideLeft'>
       <div className='titulosSearcher'>
           <span>Buscador</span>
       </div>
-      <form className='formSearcher' action="">
+      <form className='formSearcher' onSubmit={updateResult} action="">
           <img className='searchingImg' src="/searching.svg" alt="" />
-          <input className='searcherInput' type="text" placeholder='¿Qué deseas escuchar?'/>
+          <input onClick={inputClickTop} className='searcherInput' type="text" placeholder='¿Qué deseas escuchar?'/>
       </form>
       <div className='contTop'>
           <span className='topVeinteText'>Top 20s</span>
@@ -21,7 +56,22 @@ export const Searcher = ({ searcher, topSongs }) => {
       {topSongs && topSongs.map((element)=>{
           return<Card key={element.id} element={element}/>
       })}
+  </div> 
+  </>
+  }
+    {onView === 'search' && 
+  <div  className='resultSearcher'>
+    <form className='formSearcher formSearcherOn' onSubmit={updateResult} action="">
+          <img onClick={inputClickSearch} className='searchingImg' src="/vector.svg" alt="" />
+          <input  value={searchState} onChange={handleValue} className='searcherInput' type="text" placeholder='¿Qué deseas escuchar?'/>
+    </form>
+    <div className='contBusReciente'>
+        <p className='busReciente'>Búsquedas Recientes:</p>
+        <hr className='hrStyleOn'/>
+    </div>
+
   </div>
+    }
 </div> 
   )
 }
