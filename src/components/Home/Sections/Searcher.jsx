264bd73bd53e65c7e368dd977d84c382304208a7
EmiMinetti <eminetti99@gmail.com>
2023-07-31 22:34:59 -0300

    busqueda final

--- src/components/Home/Sections/Searcher.jsx
+++ src/components/Home/Sections/Searcher.jsx
@@ -1,89 +1,129 @@
-import React, { useState } from 'react'
-import { Card } from '../Card'
-import { SearchResult } from '../../SearchResult'
-import { useAuth } from '../../../contexts/authContext';
+import React, { useState } from "react";
+import { Card } from "../Card";
+import { SearchResult } from "../../SearchResult";
+import { useAuth } from "../../../contexts/authContext";
+import GenderItem from "../../MusicaContextual/GenderItem";
 
 export const Searcher = ({ searcher, topSongs }) => {
+  const [searchState, setSearchState] = useState("");
+  const [result, setResult] = useState([]);
+  const [onView, setOnView] = useState("top");
+  const [searched, setSearched] = useState(false);
 
-    const [searchState, setSearchState] = useState('');
-    const [result, setResult] = useState([]);
-    const [onView, setOnView] = useState('top');
+  const { user } = useAuth();
 
-    const {user} = useAuth();
+  const updateResult = (e) => {
+    e.preventDefault();
+    const searchQuery = encodeURIComponent(searchState);
+    fetch(`http://localhost:3000/songs/search?query=${searchQuery}`, {
+      headers: { "auth-token": user },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setSearched(true);
+      });
+  };
 
-    const updateResult = (e) => {
-        e.preventDefault()
-        const searchQuery = encodeURIComponent(searchState)
-        fetch(`http://localhost:3000/songs/search?query=${searchQuery}`, {
-            headers:{'auth-token':user},
-        })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-        })
-    }
+  const handleValue = (e) => {
+    const value = e.target.value;
+    setSearchState(value);
+  };
 
-    const handleValue = (e) => {
-       const value = e.target.value
-       setSearchState(value)
-    }
+  const inputClickTop = () => {
+    setOnView("search");
+  };
 
-    const inputClickTop = () =>{
-        setOnView('search')
-    }
+  const inputClickSearch = () => {
+    setOnView("top");
+  };
 
-    const inputClickSearch = () =>{
-        setOnView('top')
-    }
+  return (
+    <div className={`contSearcher${searcher}`}>
+      {onView === "top" && (
+        <>
+          <div className="mainSearcher animation__insideLeft">
+            <div className="titulosSearcher">
+              <span>Buscador</span>
+            </div>
+            <form className="formSearcher" onSubmit={updateResult} action="">
+              <img className="searchingImg" src="/searching.svg" alt="" />
+              <input
+                onClick={inputClickTop}
+                className="searcherInput"
+                type="text"
+                placeholder="¿Qué deseas escuchar?"
+              />
+            </form>
+            <div className="contTop">
+              <span className="topVeinteText">Top 20s</span>
+              <hr className="hrStyle" />
+            </div>
+          </div>
+          <div className="contCards animation__insideRight">
+            {topSongs &&
+              topSongs.map((element) => {
+                return <Card key={element.id} element={element} />;
+              })}
+          </div>
+        </>
+      )}
+      {onView === "search" && (
+        <div className="resultSearcher">
+          <form
+            className="formSearcher formSearcherOn"
+            onSubmit={updateResult}
+            action=""
+          >
+            <img
+              onClick={inputClickSearch}
+              className="searchingImg"
+              src="/vector.svg"
+              alt=""
+            />
+            <input
+              value={searchState}
+              onChange={handleValue}
+              className="searcherInput"
+              type="text"
+              placeholder="¿Qué deseas escuchar?"
+            />
+          </form>
+          {!searched ? (
+            <div className="contBusReciente">
+              <div className="contTopBR">
+                <p className="busReciente">Búsquedas Recientes:</p>
+                <hr className="hrStyleOn" />
+              </div>
 
-
-  return (<div className={`contSearcher${searcher}`}>
-    {onView === 'top' && 
-    <>
-  <div className='mainSearcher animation__insideLeft'>
-      <div className='titulosSearcher'>
-          <span>Buscador</span>
-      </div>
-      <form className='formSearcher' onSubmit={updateResult} action="">
-          <img className='searchingImg' src="/searching.svg" alt="" />
-          <input onClick={inputClickTop} className='searcherInput' type="text" placeholder='¿Qué deseas escuchar?'/>
-      </form>
-      <div className='contTop'>
-          <span className='topVeinteText'>Top 20s</span>
-          <hr className='hrStyle'/>
-      </div>
-  </div>
-  <div className='contCards animation__insideRight'>
-      {topSongs && topSongs.map((element)=>{
-          return<Card key={element.id} element={element}/>
-      })}
-  </div> 
-  </>
-  }
-    {onView === 'search' && 
-  <div  className='resultSearcher'>
-    <form className='formSearcher formSearcherOn' onSubmit={updateResult} action="">
-          <img onClick={inputClickSearch} className='searchingImg' src="/vector.svg" alt="" />
-          <input  value={searchState} onChange={handleValue} className='searcherInput' type="text" placeholder='¿Qué deseas escuchar?'/>
-    </form>
-    <div className='contBusReciente'>
-        <div className='contTopBR'>
-            <p className='busReciente'>Búsquedas Recientes:</p>
-            <hr className='hrStyleOn'/>
+              <div className="recienteLista">
+                <SearchResult />
+                <SearchResult />
+                <SearchResult />
+                <SearchResult />
+                <SearchResult />
+              </div>
+            </div>
+          ) : (
+            <div className="contBusquedaFinal">
+              <div className="contFiltrosBusqueda">
+                <GenderItem Text='Top' Active />
+                <GenderItem Text='Canciones'/>
+                <GenderItem Text='Albumes'/>
+                <GenderItem Text='Artistas'/>
+                <GenderItem Text='Playlist'/>
+                <GenderItem Text='Perfiles'/>
+              </div>
+              <p>Resultado sugerido:</p>
+              <SearchResult firstResult />
+              <SearchResult />
+              <SearchResult />
+              <SearchResult />
+              <SearchResult />
+            </div>
+          )}
         </div>
-        <div className='recienteLista'>
-            <SearchResult/>
-            <SearchResult/>
-            <SearchResult/>
-            <SearchResult/>
-            <SearchResult/>
-
-        </div>
+      )}
     </div>
-    
-
-  </div>
-    }
-</div> 
-  )
-}
+  );
+};
--- src/components/MusicaContextual/GenderItem.jsx
+++ src/components/MusicaContextual/GenderItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function GenderItem({ Text, Active }) {
+
+function GenderItem({ Text, Active, handleClick }) {
 
 
   return (
-    <div className="contextual__gender">
-    <span className="contextual__gender-text" onClick={Active}>{Text}</span>
+    <div className={`contextual__gender ${Active?"active":""}`} onClick={handleClick}>
+    <span className="contextual__gender-text" >{Text}</span>
   </div>
   )
 }
